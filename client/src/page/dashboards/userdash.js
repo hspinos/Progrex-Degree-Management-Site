8306fabf640b7c09d8c9bd877306913ff90ea256
Jonathan Nguyen <jnganguyen3@gmail.com>
2022-02-24 16:53:05 -0500

    successfully created a general layout 4 userdash

--- client/src/page/dashboards/userdash.js
+++ client/src/page/dashboards/userdash.js
@@ -1,8 +1,12 @@
 export default function UserDash() {
   return (
     <div className="grid grid-cols-2 h-full">
-      <div>gameboard div</div>
-      <div>other dash components div</div>
+      <div className="flex items-center">
+        <div className="text-5xl font-semibold">gameboard div</div>
+      </div>
+      <div className="flex items-center">
+        <div className="text-5xl font-semibold">other dash components div</div>
+      </div>
     </div>
   )
 }
