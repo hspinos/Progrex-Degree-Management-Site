dd0dfbfbce56ccc0410c9756bbe7913b43e3db86
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2022-03-08 14:34:28 -0500

    minor UI changes

--- client/src/components/docmodalunsigned.js
+++ client/src/components/docmodalunsigned.js
@@ -20,7 +20,7 @@ function DocModalUnsigned() {
                     </div>
                     <div id="modalFooter" className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-stone-700">
                         <button id='signButton' data-modal-toggle="unsignedModal" type="button" className="text-white bg-red-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-800">Sign Now</button>
-                        <button data-modal-toggle="unsignedModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Cancel</button>
+                        <button data-modal-toggle="unsignedModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-stone-800 dark:text-gray-300 dark:border-stone-600 dark:hover:text-white dark:hover:bg-stone-700">Cancel</button>
                     </div>
                 </div>
             </div>
