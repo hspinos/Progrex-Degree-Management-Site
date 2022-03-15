f1faf524947c6640881f516d48ecc0b63945e31f
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2022-03-15 16:49:20 -0400

    added document signing validation

--- server/controllers/documentcontroller.js
+++ server/controllers/documentcontroller.js
@@ -4,53 +4,61 @@ documentModel = new Document();
 const mongoose = require('mongoose');
 
 exports.test_document_endpoint = function (req, res) {
-    res.send('This is the test_document_endpoint');
+  res.send('This is the test_document_endpoint');
 }
 
 exports.get_documents = async function (req, res) {
-    let query = Document.find({});
-    query.exec(function (err, document) {
-        if (err) return handleError(err);
-        res
-          .status(200)
-          .send(document)
-      });
+  let query = Document.find({});
+  query.exec(function (err, document) {
+    if (err) return handleError(err);
+    res
+      .status(200)
+      .send(document)
+  });
 }
 
 exports.document_detail = async function (req, res) {
-      const document = await Document.findById(req.params.id);
-      res
-        .status(200)
-        .send(document);
+  const document = await Document.findById(req.params.id);
+  res
+    .status(200)
+    .send(document);
 }
 
 exports.set_document_signed = async function (req, res) {
-    const document = await Document.findById(req.params.docId);
-    document.usersSigned.push({ signerId: mongoose.Types.ObjectId(req.params.userId) });
+  let userId = mongoose.Types.ObjectId(req.params.userId);
+  const document = await Document.findById(req.params.docId);
+  if (document.usersSigned.find(signData => signData.signerId.equals(userId))) {
+    res
+      .status(401)
+      .send("Error, user has already signed this document.");
+  } else {
+    document.usersSigned.push({ signerId: userId });
     await document.save();
     res
-          .json(document)
-          .status(200)
-          .send();
+      .json(document)
+      .status(200)
+      .send();
+  }
+
 }
 
 exports.create_document = async function (req, res) {
-    try {
-        console.log("create document endpoint called");
-        const document = new Document({
-          name: req.body.name,
-          description: req.body.description,
-          powerFormUrl: req.body.powerFormUrl,
-        });
-        await document.save();
-        res
-          .json(document)
-          .status(200)
-          .send();
-      } catch (err) {       // User most likely already exists
-        console.error(err);
-        res
-          .status(401)
-          .send();
-      }
+  try {
+    console.log("create document endpoint called");
+    const document = new Document({
+      name: req.body.name,
+      description: req.body.description,
+      powerFormUrl: req.body.powerFormUrl,
+    });
+    await document.save();
+    res
+      .json(document)
+      .status(200)
+      .send();
+  } catch (err) {       // User most likely already exists
+    console.error(err);
+    res
+      .status(401)
+      .send();
+  }
 }
