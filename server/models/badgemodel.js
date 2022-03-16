15cc6358a132ea610c1462077e3c7b07eae6fff8
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2022-03-16 13:54:30 -0400

    removed redundant ID attributes for models

--- server/models/badgemodel.js
+++ server/models/badgemodel.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const badgeSchema = new Schema(
   {
-    badgeId: {type: String, required: true, maxLegnth: 40, unique: true, dropDups: true},
     isApproved: {type: Boolean, required: true,},
     badgeName: {type: String, required: true, maxLegnth: 40, unique: true, dropDups: ture}
   }
