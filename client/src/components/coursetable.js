a4a99d7b33cf19482137f8e49aab0fc434bc5230
eh04987 <eh04987@georgiasouthern.edu>
2022-03-21 23:15:13 -0400

    base level progress page, shows table with data

--- client/src/components/coursetable.js
+++ client/src/components/coursetable.js
@@ -1,8 +1,44 @@
-import React from "react"
-function couresTable() {
-    <div>
-        <div className="table-cell text-left">Course</div>
-        <div className="table-cell text-left">Course Year</div>
-    </div>
-                 
+import React, { useState } from "react"
+import data from "./mock-data.json"
+
+
+function CourseTable() {
+    const [contacts, setContacts] = useState(data);
+    return (
+
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Course Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Course ID
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Course Year
+                        </th>
+                    </tr>
+                </thead>
+                <tbody> 
+                    {contacts.map((contact) => (
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {contact.name}
+                        </th>
+                        <td class="px-6 py-4">
+                            {contact.id}
+                        </td>
+                        <td class="px-6 py-4">
+                            {contact.courseYear}
+                        </td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
+export default CourseTable;
--- client/src/page/progress/userprogress.js
+++ client/src/page/progress/userprogress.js
@@ -1,42 +1,12 @@
 import React from "react";
+import CourseTable from '../../components/coursetable'
 
 let fillerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec feugiat lorem, sed efficitur nulla. Donec elementum augue ac ex suscipit fermentum.';
 
 function UserProgress() {
-    let courses=[{
-        name:"Course 1",
-        couresYear:"2018",
-        id:1,
-      },{
-        name:"Course 2",
-        courseYear:"2019",
-        id:2,
-      },{
-        name:"Course 3",
-        courseYear:"2029",
-        id:3,
-      },{
-        name:"Course 4",
-        courseYear:"2020",
-        id:4,
-      },{
-        name:"Course 5",
-        courseYear:"2019",
-        id:5,
-      },{
-        name:"Course 6",
-        courseYear:"2020",
-        id:6,
-      }]
-
     return (
-        <div className="table w-full">
-            <div className="table-header-group">
-                <div className="table-row">
-                    <div className="table-cell text-left">Course</div>
-                    <div className="table-cell text-left">Coure Year</div>
-                </div>
-            </div>    
+        <div>
+            <CourseTable/>
         </div>
     );
     }
