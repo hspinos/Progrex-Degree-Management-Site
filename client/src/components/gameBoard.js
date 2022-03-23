282f712a7ce53b90a6655b4d1455f01e00ef5b9a
hspinos <hspinos@gmail.com>
2022-03-23 09:04:38 -0400

    Commented game board

--- client/src/components/gameBoard.js
+++ client/src/components/gameBoard.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
+//Unity context object is from react-unity-webgl library that acts as brain for embedded WebGL build.
+//Build files must be stored in the public folder
 const config = new UnityContext({
     loaderUrl: "Build/gameboard.loader.js",
     dataUrl: "Build/gameboard.data",
@@ -8,15 +10,22 @@ const config = new UnityContext({
     codeUrl: "Build/gameboard.wasm"
 })
 
+//Handle retrieving data from the data base and spawning all avatars on gameboard based on data returned
 function spawnAvatar(){
+    //Insert logic for calling database here
+    //axios.get call to get all student objects
+    //then call config.send within a for loop for i times where i = num of students returned
+    //for (int i = 0; i <= axios.getStudents.length; i++)
+    //  config.send("Spawner", "spawnAvatar", axios.getStudents[i]);
     config.send("Spawner", "spawnAvatar", "{\"FName\":\"Jonathan\", \"LName\" : \"Nguyen\", \"position\" : 1, \"avatarNum\" : 1}");
 }
 
+//React component to be called to spawn the gameboard
 function GameBoard(){
 
+    //Uses state to ensure the gameboard is loaded before calling method to spawn avatars
     const [isLoaded, setIsLoaded] = useState(false);
 
-    
     useEffect(function () {
       config.on("loaded", () => {
         setIsLoaded(true);
@@ -27,17 +36,19 @@ function GameBoard(){
     
     
     return (
-        <Unity 
-            unityContext={config}
-            className="w-full"
-            style={{
-                visibility: isLoaded ? "visible" : "hidden",
-                height: "100%",
-                width: "100%",
-                border: "6px solid #00E676",
-                background: "grey",
-            }} 
-        />
+        <div>
+            <Unity 
+                unityContext={config}
+                style={{
+                    visibility: isLoaded ? "visible" : "hidden",
+                    height: "100%",
+                    width: "100%",
+                    border: "6px solid #00E676",
+                    background: "grey",
+                }} 
+            />
+        </div>
+
     );
 }
 
