63c56a94d7db1ac510ebebae0c2a3d3fbe0dc860
Jonathan Nguyen <jnganguyen3@gmail.com>
2022-04-04 21:51:04 -0400

    made a change to the usermodel

--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -5,7 +5,8 @@ const userSchema = new Schema(
   {
     fName: { type: String, required: true, maxLength: 40 },
     lName: { type: String, required: true, maxLength: 40 },
-    username: { type: String, required: true, maxLength: 40, unique: true, dropDups: true},
+    isAdmin: { type: Boolean, required: true, default: false },
+    username: { type: String, required: true, maxLength: 40, unique: true, dropDups: true },
     password: { type: String, required: true },
     position: { type: String, requried: true },
     avatarNum: { type: String, required: true }
