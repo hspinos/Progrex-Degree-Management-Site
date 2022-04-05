29269c3e58218c03cdb1e817e66d94550d390ecc
hspinos <hspinos@gmail.com>
2022-04-05 11:52:01 -0400

    data change

--- server/controllers/gameBoardController.js
+++ server/controllers/gameBoardController.js
@@ -3,6 +3,8 @@ let gameBoard = require('../models/gameboardmodel');
 
 const fakeData = [
     {FName: "Jonathan", LName: "Nguyen", position: 0, avatarNum: 1},
+    {FName: "Hayden", LName: "Spinos", position: 0, avatarNum: 1},
+    {FName: "Elias", LName: "Binchamo", position: 5, avatarNum: 1},
     {FName: "Jonathan", LName: "Nguyen", position: 4, avatarNum: 1},
     {FName: "Hayden", LName: "Spinos", position: 5, avatarNum: 1},
     {FName: "Nate", LName: "Agcaoili", position: 5, avatarNum: 1},
