d7c867f283ce8f67455f76ab6a70822abdb2e74e
Jonathan Nguyen <jnganguyen3@gmail.com>
2022-04-05 14:19:01 -0400

    added flex to updateuserinfo

--- client/src/page/user/updateuserinfo.js
+++ client/src/page/user/updateuserinfo.js
@@ -16,30 +16,24 @@ function UpdateUserPage() {
   const [lName, setLName] = useState('');
 
   return (
-    <div className="h-full w-full">
-      <div className="grid justify-center content-center">
+    <div className="flex flex-1 h-full justify-center content-center">
+      <div>
         <form>
           <div>
-            <label>
-              First Name:
-            </label>
+            <label>First Name:</label>
             <br />
-            <input className="rounded-md" type="text" name="fName" onChange={e => setFName(e.target.value)} />
+            <input className="rounded-md text-black" type="text" name="fName" onChange={e => setFName(e.target.value)} />
           </div>
           <div>
-            <label>
-              Last Name:
-            </label>
+            <label>Last Name:</label>
             <br />
-            <input className="rounded-md" type="text" name="lName" onChange={e => setLName(e.target.value)} />
+            <input className="rounded-md text-black" type="text" name="lName" onChange={e => setLName(e.target.value)} />
           </div>
           <br />
           <div className="bg-green-500 text-center rounded-md p-2">
             <input type="submit" value="Submit Changes" />
           </div>
-
         </form>
-
       </div>
     </div>
   )
