02689fba21295b92b27b16ed9e02d0e11141f264
hspinos <hspinos@gmail.com>
2022-04-19 23:50:38 -0400

    Removed unnecessary gameboard server config

--- server/routes/GameBoardRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const gameBoardRouter = express.Router();
-
-const gameBoardController = require('../controllers/gameBoardController');
-
-gameBoardRouter.get('/list', gameBoardController.get_users);
-
-module.exports = gameBoardRouter;
